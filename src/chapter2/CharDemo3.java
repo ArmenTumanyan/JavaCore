0cba83871d1a4b55db2db7b9fb2c7ba7070390b5
Armen <armen.tumanyan11@gmail.com>
2022-12-19 23:22:42 +0400

    IfForExample

---
+++ src/chapter2/CharDemo3.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class CharDemo3 {
+
+    public static void main(String[] args) {
+
+char ch1, ch2;
+ch1 = 'a';
+ch2 = 'b';
+        System.out.print("a, b");
+    }
+
+}
---
+++ src/homeworks/FigurePainter2.java
@@ -0,0 +1,21 @@
+package homeworks;
+
+public class FigurePainter2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            System.out.print("* ");
+        }
+
+        System.out.println();
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < j; k++) {
+
+            }
+
+        }
+        System.out.print("* ");
+    }
+}
+
