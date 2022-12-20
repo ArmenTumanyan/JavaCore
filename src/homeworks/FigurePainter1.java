81ae487e256936aee8d6433fa76a40ed89ef57f2
Armen <armen.tumanyan11@gmail.com>
2022-12-20 13:41:44 +0400

    IfForExample2

--- src/homeworks/FigurePainter1.java
+++ src/homeworks/FigurePainter1.java
@@ -1,31 +1,4 @@
 package homeworks;
 
-
 public class FigurePainter1 {
-
-    public static void main(String[] args) {
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print(" ");
-
-            }
-            for (int k = 0; k <= i; k++) {
-                System.out.print("* ");
-            }
-                System.out.println();
-
-
-
-
-
-        }
-
-    }
 }
-
-
-
-
-
-
