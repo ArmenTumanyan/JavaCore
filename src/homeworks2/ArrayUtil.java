a428ae615d6bced8ec0296ce044e917b977593a8
Armen <armen.tumanyan11@gmail.com>
2022-12-23 19:51:23 +0400

    homeworks2

--- src/homeworks2/ArrayUtil.java
+++ src/homeworks2/ArrayUtil.java
@@ -1,6 +1,6 @@
 package homeworks2;
 
-public  class ArrayUtil {
+public class ArrayUtil {
 
     public static void main(String[] args) {
         int[] myArray = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
@@ -9,9 +9,31 @@ public  class ArrayUtil {
             System.out.println(myArray[i]);
         }
 
+        System.out.println();
+        {
+
+
+            int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+
+            int max = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > max) max = array[i];
+            }
+            System.out.println(max);
+        }
+        System.out.println();
 
         {
 
+
+            int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+
+            int min = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < min) min = array[i];
+            }
+            System.out.println(min);
+
         }
     }
-}
+    }
--- src/homeworks2/FigurePainter2.java
+++ src/homeworks2/FigurePainter2.java
@@ -22,5 +22,26 @@ public class FigurePainter2 {
             }
             System.out.println();
         }
-    }
-}
+        System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j <= i; j++) {
+                            System.out.print("   ");
+                        }
+
+                        for (int j = 4; j > i; j--) {
+                            {
+                                System.out.print(" " + "* ");
+                            }
+                        }
+                        System.out.println();
+                    }
+                }
+            }
+
+
+
+
+
+
+
