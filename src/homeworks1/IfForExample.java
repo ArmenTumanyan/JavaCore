87afb595fe5a38ee49df03ee934f4db07491ba84
Armen <armen.tumanyan11@gmail.com>
2022-12-26 15:31:10 +0400

    homeworks1

--- src/homeworks1/IfForExample.java
+++ src/homeworks1/IfForExample.java
@@ -3,32 +3,31 @@ package homeworks1;
 public class IfForExample {
 
     public static void main(String[] args) {
-        int x = 20;
-        int y = 50;
-        if (x < y) ;
+        int a = 20;
+        int b = 50;
+        if (a < b) ;
         {
-            System.out.println(y);
+            System.out.println(b);
         }
         System.out.println();
+        a = 5;
+        b = 10;
 
-
-        int i = 5;
-        int j = 10;
-
-        for (i = 5; i <= j; i++) {
-            System.out.print(i);
-
-    }
+        for (a = 5; a <= b; a++) {
+            System.out.print(a);
+        }
 
         System.out.println();
 
-            char ch1, ch2;
-            int a = 10, b = 50;
-            ch1 = 'a';
-            ch2 = 'b';
-            System.out.println("ch1 содержит " + a);
-            System.out.println("ch2 содержит " + b);
+            char c = (char) a;
+        System.out.println(a);
 
+        System.out.println();
 
+    if(a == b) {
+        System.out.println("a = b");
+    }else {
+        System.out.println("a != b");
+    }
     }
 }
