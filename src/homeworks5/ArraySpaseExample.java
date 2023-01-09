3edca07fbac13863150315f618084cfc39b31e2b
Armen <armen.tumanyan11@gmail.com>
2023-01-09 20:39:09 +0400

    homeworks5

---
+++ src/homeworks5/ArraySpaseExample.java
@@ -0,0 +1,33 @@
+package homeworks5;
+
+public class ArraySpaseExample {
+
+
+    public static void main(String[] args) {
+        char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
+        int firstIndex = 0;
+        int lastIndex = 0;
+        for (int i = 0; i < spaceArray.length; i++) {
+            if (spaceArray[i] != ' ') {
+                firstIndex = i;
+                break;
+            }
+
+        }
+        for (int j = spaceArray.length - 1; j > 0; j--) {
+            if (spaceArray[j] != ' ') {
+                lastIndex = j;
+                break;
+            }
+
+        }
+        char[] newspaseArray = new char[lastIndex - firstIndex + 1];
+
+        for (int i = 0; i < newspaseArray.length; i++) {
+            newspaseArray[i] = spaceArray[firstIndex++];
+            System.out.print(newspaseArray[i]);
+        }
+
+
+    }
+}
