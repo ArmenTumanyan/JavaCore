b53d94f296580ca227cebb04e1c07687db7198b5
Armen <armen.tumanyan11@gmail.com>
2023-01-16 14:50:33 +0400

    homeworks7

---
+++ src/homeworks7/Calculator.java
@@ -0,0 +1,33 @@
+package homeworks7;
+
+public class Calculator {
+
+    double plus(double a, double b) {
+        double c = (a + b);
+        return c;
+       // System.out.println();
+    }
+
+    double minus(double a, double b) {
+        double c = (a - b);
+        return c;
+    }
+
+    double divide(double a, double b) {
+        double c = (a / b);
+        if (b == 0) {
+            System.out.println("թիվը 0-ի չի բաժանվում");
+        }
+        return c;
+    }
+
+    double multiply(double a, double b) {
+        double c = (a * b);
+        return c;
+     //   System.out.println();
+    }
+
+}
+
+
+
---
+++ src/homeworks7/CalculatorDemo.java
@@ -0,0 +1,20 @@
+package homeworks7;
+
+public class CalculatorDemo {
+
+    public static void main(String[] args) {
+        Calculator col = new Calculator();
+
+        col.plus(10, 3);
+        System.out.println(col.plus(10, 3));
+
+        col.minus(10, 5);
+        System.out.println(col.minus(10, 3));
+
+        col.divide(8, 4);
+        System.out.println(col.divide(8, 4));
+
+        col.multiply(5, 2);
+        System.out.println(col.multiply(5, 2));
+    }
+}
